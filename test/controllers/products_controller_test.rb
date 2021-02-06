439d08762a622859b36ca46ade85d698c6c15165
yosnak13 <yosnak13@gmail.com>
2021-02-06 21:19:25 +0900

    create producs controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
 end
