ef10cf1ee323a235722b23149a3aff46b0b62806
yosnak13 <yosnak13@gmail.com>
2021-02-07 16:50:50 +0900

    商品登録画面、メソッド作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,11 +12,19 @@ PER = 20
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :maker_name)
   end
 end
