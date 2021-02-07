8e57ea3faeb4bc4cbfbfe8aeb5c151d0ab951baa
yosnak13 <yosnak13@gmail.com>
2021-02-07 16:29:05 +0900

    paginate 実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,22 @@
 class ProductsController < ApplicationController
+PER = 20
+
   def index
     @products = Product.all
+    # @products = Product.find(params[:id])
+    @products = Product.page(params[:page]).per(PER)
+  end
+
+  def new
+    @products = Product.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "products#index"
+  root 'products#index'
   resources :products
 end
