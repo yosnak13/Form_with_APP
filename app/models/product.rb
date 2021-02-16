cd2c2be25a8d7bd1132b519159fe10c72b7d8420
yosnak13 <yosnak13@gmail.com>
2021-02-16 23:10:56 +0900

    不要なコードの排除

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,2 @@
 class Product < ApplicationRecord
-  # def self.search_product(keyword)
-  #   if keyword =! nil
-  #     Product.where("name LIKE?", "%#{keyword}%")
-  #   else
-  #     Product.all
-  #   end
-  # end
-  # scope :search_product, -> (keyword) { where("name LIKE?", "%#{keyword}%") if keyword.present? }
 end
